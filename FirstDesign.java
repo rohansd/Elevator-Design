7fee80ecbd2f31dd5e7abe2b92eeb9513af5bda5
Rohan <78194318+rohansd@users.noreply.github.com>
2022-05-26 10:04:28 -0700

    Update FirstDesign.java

--- FirstDesign.java
+++ FirstDesign.java
@@ -1,1 +1,35 @@
+import java.util.PriorityQueue;
 
+public class Elevator {
+
+    int currentFloor;
+    Direction direction;
+
+    public Elevator(int currentFloor) {
+        this.currentFloor = currentFloor;
+        this.direction = Direction.IDLE;
+    }
+
+    public void sendUpRequest(Request upRequest) {
+    }
+
+    public void sendDownRequest(Request downRequest) {
+    }
+
+    public void run() {
+    }
+
+    private void processRequests() {
+    }
+
+    private void processUpRequest() {
+    }
+
+    private void processDownRequest() {
+    }
+
+
+    public static void main(String[] args) {
+    }
+
+}
